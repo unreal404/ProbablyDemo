d32980e2657703383794dab0bcb36d895e5a9b50
zhouwei <404unread@gmail.com>
2018-10-24 17:29:08 +0800

    fix UI

--- app/src/main/java/com/example/a55300/animationdemo/activity/MainActivity.java
+++ app/src/main/java/com/example/a55300/animationdemo/activity/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.a55300.animationdemo.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.a55300.animationdemo.R;
 import com.example.a55300.animationdemo.adapter.MainAdapter;
 import com.example.a55300.animationdemo.bean.TypeBean;
@@ -15,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BaseQuickAdapter.OnItemClickListener{
 
     private MainAdapter adapter;
     private ActivityMainBinding binding;
@@ -35,13 +38,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void initData(){
         adapter = new MainAdapter(getData(),this);
+        adapter.setOnItemClickListener(this);
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerView.setAdapter(adapter);
     }
 
     private List<TypeBean> getData(){
         typeBeanList.add(new TypeBean("自定义圆环进度条", 1));
+        typeBeanList.add(new TypeBean("占个地的", 1));
+        typeBeanList.add(new TypeBean("占个地的", 1));
+        typeBeanList.add(new TypeBean("占个地的", 1));
 
         return typeBeanList;
+    }
+
+    @Override
+    public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+        switch (typeBeanList.get(position).getType()) {
+            case 1: toNextTopic(ProgressRingActivity.class,typeBeanList.get(position).getTitle()); break;
+            default:
+        }
+    }
+
+    private void toNextTopic(Class<?> cls, String title) {
+        if (title != null) {
+            Intent intent = new Intent(MainActivity.this, cls);
+            intent.putExtra("title", title);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/a55300/animationdemo/activity/ProgressRingActivity.java
+++ app/src/main/java/com/example/a55300/animationdemo/activity/ProgressRingActivity.java
@@ -1,9 +1,13 @@
 package com.example.a55300.animationdemo.activity;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.a55300.animationdemo.R;
 import com.example.a55300.animationdemo.databinding.ProgressRingBinding;
@@ -12,13 +16,37 @@ import com.example.a55300.animationdemo.databinding.ProgressRingBinding;
 
 public class ProgressRingActivity extends AppCompatActivity{
 
-    ProgressRingBinding binding;
+    private ProgressRingBinding binding;
+    private String title;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initView();
+        initListener();
+        binding.includeTitle.tvTitle.setText(title);
+    }
+
+    private void initView() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            title = getIntent().getStringExtra("title");
+        }
+
         binding = DataBindingUtil.setContentView(this, R.layout.progress_ring);
 
         binding.pring.setProgress(100);
     }
+
+    private void initListener() {
+        binding.includeTitle.llTitleLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
 }
